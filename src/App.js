fc4602622d89177521e87b4d31d0916a040e5731
unknown <hamayoon.47611@iqra.edu.pk>
2023-03-15 14:17:27 +0500

    added discover file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Discover from "./components/Discover";
 
 function App() {
   return (
     <div className="App">
-    
+      <Routes>
+        <Route path="/" element={<Discover />} />
+      </Routes>
     </div>
   );
 }
